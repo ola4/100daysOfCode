7c140644eb510b74ee6ea701d0a712133f903508
Aleksandra Nowak <ola@copenhagensoftware.com>
2020-05-31 12:12:29 +0200

    test push

---
+++ FSharpPlayground/File.fs
@@ -0,0 +1,12 @@
+﻿module File
+
+    let whatever x = x*x
+    
+    let sampleRange = [0..99]
+
+    let squares (_x:list<int>) = [for el in _x -> (el,el*el)]
+
+    let result = squares sampleRange
+
+   printfn "The table of squares from 0 to 99 is:\n%A" result
+
